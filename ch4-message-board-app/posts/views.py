07372bc9fbd433328af4187275b1a04267df8674
Festus Aboagye <163061224+thecollekta@users.noreply.github.com>
2024-08-26 18:42:15 +0000

    Delete ch4-message-board-app directory

--- ch4-message-board-app/posts/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Post
-
-class HomePageView(ListView):
-    model = Post
-    template_name = "home.html"
